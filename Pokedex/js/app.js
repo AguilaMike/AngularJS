4614559beb576da21b6d6b3981e01ad9c2b6a260
AguilaMike <davidmiguelh@gmail.com>
2014-10-30 22:15:59 -0600

    Creamos un filtro personalizado para resolver las rutas de los pokemons [We create a custom filter by resolve pokemon's path]

--- Pokedex/js/app.js
+++ Pokedex/js/app.js
@@ -43,4 +43,15 @@
             return tabs.tab === tab;
         }
     }]);
+    
+    app.filter('imageify', [function () {
+        /*
+        *   Filtro para establecer las  imagenes de los pokemons
+        *   Filter to set pokemons' images
+        */
+        return function (input, ext) {
+            var url = "img/pokemons/" + input.toLowerCase() + "." + (ext || "jpg");
+            return url;
+        };
+    }]);
 }());
