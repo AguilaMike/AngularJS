916bee21e7443eeae42f3f5fabf67d0e304f37ea
AguilaMike <davidmiguelh@gmail.com>
2014-11-09 09:06:17 -0600

    Incorporamos filtros sobre filtros (We created fiter in by filter)

--- Pokedex/js/filter.js
+++ Pokedex/js/filter.js
@@ -1,13 +1,30 @@
 (function() {
     var app = angular.module('pokedex.filters', []);
 
-    app.filter('imageify', [function () {
+    app.filter('normalize', [function () {
+        /*
+        *   Filtro para normalizar el texto
+        *   Filter by normalize words
+        */
+        return function(input) {
+            if (!input) return '';
+
+            input = input
+                        .replace('♀', 'f')
+                        .replace('♂', 'm')
+                        .replace(/\W+/g, '');
+
+            return input.toLowerCase();
+        };
+    }]);
+
+    app.filter('imageify', ['$filter', function ($filter) {
         /*
         *   Filtro para establecer las  imagenes de los pokemons
         *   Filter to set pokemons' images
         */
         return function (input, ext) {
-            var url = "img/pokemons/" + input.toLowerCase() + "." + (ext || "jpg");
+            var url = "img/pokemons/" + $filter('normalize')(input) + "." + (ext || "jpg");
             return url;
         };
     }]);
